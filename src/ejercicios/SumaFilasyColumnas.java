7354213fc3d86ef94667197a1bed8c2a7a900e93
sitotamario3 <sitotamario3@gmail.com>
2026-03-09 14:57:26 +0100

    SumarFilasyColumnas (Matrices)

--- src/ejercicios/SumaFilasyColumnas.java
+++ src/ejercicios/SumaFilasyColumnas.java
@@ -3,7 +3,23 @@ package ejercicios;
 public class SumaFilasyColumnas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		int sum = 0;
+		
+		int[][] matriz = new int[3][3];
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				int valor = (int) (Math.random() * 100);
+				matriz[i][j] = valor;
+				sum += matriz[i][j];
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("La suma de los valores de la matriz es de: " + sum);
 
 	}
 
