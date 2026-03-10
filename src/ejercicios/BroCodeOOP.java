48d79df7ba816c69306f42ccbe4468df92540319
sitotamario3 <sitotamario3@gmail.com>
2026-03-10 15:20:17 +0100

    BroCode POO

---
+++ src/ejercicios/BroCodeOOP.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class BroCodeOOP {
+
+	public static void main(String[] args) {
+		
+		
+		// Object = An entity that holds data (attributes)
+		// 			and can perform actions (methods)
+		//			It is a reference data type
+
+	}
+
+}
