44c88876a2c902674b0444fde0c50a71dad59a00
sito03 <sitotamario3@gmail.com>
2026-04-08 20:53:38 +0200

    primer dia excepciones correccion

--- src/tercerTrimestreExcepciones/InputMismatchException.java
+++ src/tercerTrimestreExcepciones/InputMismatchException.java
@@ -1,9 +1,26 @@
 package tercerTrimestreExcepciones;
 
+import java.util.Scanner;
+
 public class InputMismatchException {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		while (true) {
+		try {
+		System.out.println("Hola, dame un numero entero por favor: ");
+		int entero = sc.nextInt();
+		System.out.println("El numero entero es valido: ");
+		} catch (InputMismatchException e) {
+			System.out.println("Por favor, introduce un valor válido");
+		}
+		
+		}
+		
+		
+		
+		sc.close();
 
 	}
 
